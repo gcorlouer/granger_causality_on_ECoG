189fae9a604a3e9bef47effa8ab0d4e1db189cfb
Guime <guillaume.corlouer@gmail.com>
2022-07-12 19:32:03 +0100

    coded time frequency analysis

---
+++ scripts/cross_time_freq_analysis.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  5 13:29:20 2022
+In this script we run cross subjects time frequency analysis
+@author: guime
+"""
+
+#%%
+import argparse
+import numpy as np
+import pandas as pd
+
+from src.input_config import args
+from src.time_frequency import compute_group_power
+
+#%% Parameters
+cohort = args.cohort
+conditions = ["Rest", "Face", "Place"]
+groups = ["R", "O", "F"]
+ngroup = len(groups)
+ncdt = len(conditions)
+power_dict = {"subject": [], "condition": [], "group": [], "power": []}
+# Command arguments
+parser = argparse.ArgumentParser()
+# Frequency space
+parser.add_argument("--sfreq", type=float, default=500 / args.decim)
+parser.add_argument("--nfreqs", type=float, default=2**3)
+parser.add_argument("--fmin", type=float, default=0.5)
+parser.add_argument("--l_freq", type=float, default=0.01)
+# Baseline correction
+# tf_mode and tf_baseline to not confuse with mode and baseline of epoching
+parser.add_argument("--mode", type=str, default="zscore")
+parser.add_argument("--tmin_baseline", type=float, default=-0.5)
+parser.add_argument("--tmax_baseline", type=float, default=0)
+tf_args = parser.parse_args()
+
+#%% For a given subject, run time frequency analysis across groups and
+# conditions
+
+
+def cross_tf_analysis(args, tf_args):
+    for subject in args.cohort:
+        for group in groups:
+            for condition in conditions:
+                power, time = compute_group_power(
+                    args, tf_args, subject=subject, group=group, condition=condition
+                )
+                power_dict["subject"].append(subject)
+                power_dict["condition"].append(condition)
+                power_dict["group"].append(group)
+                power_dict["power"].append(power)
+    df_power = pd.DataFrame(power_dict)
+    return df_power
+
+
+def main():
+    fname = "tf_power_dataframe.pkl"
+    df_power = cross_tf_analysis(args, tf_args)
+    fpath = args.results_path
+    fpath = fpath.joinpath(fname)
+    df_power.to_pickle(fpath)
+
+
+if __name__ == "__main__":
+    main()
--- src/time_frequency.py
+++ src/time_frequency.py
@@ -1,27 +1,28 @@
+# Import numpy
+import numpy as np
+# Import classes
+from src.preprocessing_lib import EcogReader, Epocher
+# Import functions
+from src.preprocessing_lib import visual_indices, parcellation_to_indices
+from mne.time_frequency import tfr_morlet
+# Import arguments
+#%% Define functions
 
 
-def visual_indices(args, subject='DiAs'):
+def compute_group_power(args, tf_args, subject='DiAs', group='R', 
+                        condition='Face'):
     """
-    Return indices of each functional group for a given subject
+    Compute power of visually responsive in a specific group epochs 
+    for time freq analysis
     Input: 
-        - data_path (string): where data of cifar project is stored 
-        - subject (string): subject name
-    Output:
-        - indices (dict): indices of each functional group
+        args: arguments from input_config
+        tf_args: arguments to run  cross_time_freq_analysis
+        subject: subject name
+        group: group of channels name
+        condition: condition name
     """
-    # Read visual channel dataframe
-    reader = EcogReader(args.data_path, subject=subject)
-    df_visual = reader.read_channels_info(fname=args.channels)
-    # Return indices of functional groups from visual channel dataframe
-    indices = parcellation_to_indices(df_visual, parcellation='group', matlab=False)
-    return indices 
-
-def compute_group_power(args, freqs, group='F', condition = 'Face', l_freq = 0.01,
-                 baseline=(-0.5, 0), mode = 'zscore'):
-    """Compute power of visually responsive in a specific group epochs 
-    for time freq analysis"""
     # Read ECoG
-    reader = EcogReader(args.data_path, subject=args.subject, stage=args.stage,
+    reader = EcogReader(args.data_path, subject=subject, stage=args.stage,
                          preprocessed_suffix=args.preprocessed_suffix,
                          epoch=args.epoch)
     raw = reader.read_ecog()
@@ -40,20 +41,32 @@ def compute_group_power(args, freqs, group='F', condition = 'Face', l_freq = 0.0
                              tmax_baseline=args.tmax_baseline, mode=args.mode)
     epochs = epocher.epoch(raw)
     # High pass filter
-    epochs = epochs.filter(l_freq=l_freq, h_freq=None)
+    epochs = epochs.filter(l_freq=tf_args.l_freq, h_freq=None)
     # Downsample
     epochs = epochs.decimate(args.decim)
     times = epochs.times
     # Pick channels
     epochs = epochs.pick(group_chans)
     # Compute time frequency with Morlet wavelet 
+    freqs = get_freqs(tf_args)
     n_cycles = freqs/2
     power = tfr_morlet(epochs, freqs, n_cycles, return_itc=False)
     # Apply baseline correction
-    baseline = (args.tmin_baseline, args.tmax_baseline)
-    print(f"\n Computing group power from morlet wavelet: rescale with {mode}")
+    baseline = (tf_args.tmin_baseline, tf_args.tmax_baseline)
+    print(f"\n Morlet wavelet: rescaled with {tf_args.mode}")
     print(f"\n Condition is {condition}\n")
-    power.apply_baseline(baseline=baseline, mode=mode)
+    power.apply_baseline(baseline=baseline, mode=tf_args.mode)
     power = power.data
-    power = np.average(power,axis=0)
+    power = np.average(power, axis=0)
     return power, times
+
+
+def get_freqs(tf_args):
+    """Get frequencies for time frequency analysis"""
+    fmin = tf_args.fmin
+    nfreqs = tf_args.nfreqs
+    sfreq = tf_args.sfreq
+    fmax = sfreq/2
+    fres = (fmax + fmin - 1)/nfreqs
+    freqs = np.arange(fmin, fmax, fres)
+    return freqs
